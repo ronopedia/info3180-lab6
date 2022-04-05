1265365e20f2caf6e1d5c3de8a011fd19b70b953
ronopedia <81117989+ronopedia@users.noreply.github.com>
2022-04-04 21:27:17 -0500

    Updated

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,19 +1,4 @@
-<template>
-    <ul class="news__list">
-        <li v-for="article in articles" class="news__item">
-        <img  :src="article.urlToImage" /> 
-        <p class="news__para"> {{ article.title }} {{article.description}} </p>  
-        </li>
-    </ul>
-    <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
-        <div class="input-group mx-sm-3 mb-2">
-            <label class="visually-hidden" for="search">Search</label>
-            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
-            <button class="btn btn-primary mb-2">Search</button>
-        </div>
-        <p>You are searching for {{ searchTerm }}</p>
-    </form>
-</template>
+<template> <div class="row"> <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center"> <div class="input-group mx-sm-3 mb-2"> <label for="search" class="visually-hidden">Search</label> <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here"> <button class="btn btn-success mb-2">Search</button> </div> <p class="fs-5 fw-light text-secondary text-center">You are searching for: {{ searchTerm }}</p> </form> <div v-for="article in articles" class="col-sm-6 col-md-4 mb-5"> <div class="card w-77 h-100 border-top-0 border-start-0 border-end-0 border-bottom border-5 border-success shadow"> <img :src="article.urlToImage" alt="article image" class="card-img-top"/> <div class="card-body"> <h5 class="card-title"> {{ article.title }} </h5> <p class="card-text"> {{ article.description }} </p> </div> </div> </div> </div></template>
 
 
 <script>
